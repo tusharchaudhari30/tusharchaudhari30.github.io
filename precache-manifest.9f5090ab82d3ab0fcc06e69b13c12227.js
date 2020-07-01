89474278208b0ed3f933661530c251677dcb3ad7
Jarvis <blazeric30@gmail.com>
2020-07-02 01:29:36 +0530

    as

---
+++ precache-manifest.9f5090ab82d3ab0fcc06e69b13c12227.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d0a7e736276dcb4db8ceca025fa313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fffa9db3a087907c9715",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "7776d76754ef600c08da",
+    "url": "/static/css/main.33baf564.chunk.css"
+  },
+  {
+    "revision": "fffa9db3a087907c9715",
+    "url": "/static/js/2.abcd1d42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.abcd1d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7776d76754ef600c08da",
+    "url": "/static/js/main.63df0934.chunk.js"
+  },
+  {
+    "revision": "27175d09b9547c6b2c09",
+    "url": "/static/js/runtime-main.2f9bb071.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
